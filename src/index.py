fe8feb12334af6ea4401f95fda124b4f1748c1a4
Michielu Menning <mich.menning@gmail.com>
2020-04-09 21:52:26 -0500

    Extract web scraping to service

--- src/index.py
+++ src/index.py
@@ -9,34 +9,12 @@ import ssl
 import urllib.request as urlrq
 import numpy as np
 from dataObj.MyStocks import MyStock
-# from services.sp500 import SP500
+from services.sp500 import SP500
 
-
-resp = urlrq.urlopen('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies',
-                     context=ssl.create_default_context(cafile=certifi.where()))
-
-html = resp.read()  # table = data[0]
-data = pd.read_html(html)
-
-table = data[0]
-sliced_table = table[1:]
-header = table.iloc[0]
-corrected_table = sliced_table.rename(columns=header)
-
-print(corrected_table.values)
-
+sp500Data = SP500["getSP500"]()
 sp500 = []
-for x in corrected_table.values:
+for x in sp500Data:
     sp500.append(MyStock(x[0], x[1], x[3]))
-    # print(stock1.getAll())
 
 for company in sp500:
     print(company.getData())
-# print(sp500)
-
-# appl = stocker.predict.tomorrow('AAPL')
-# print(appl)
-# appl.plot_stock()
-
-print("IN HERE :")
-# SP500.getSp500()
---
+++ src/services/sp500.py
@@ -0,0 +1,21 @@
+import certifi
+import ssl
+import urllib.request as urlrq
+import pandas as pd
+
+
+def getSP500():
+    resp = urlrq.urlopen('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies',
+                         context=ssl.create_default_context(cafile=certifi.where()))
+    html = resp.read()
+    data = pd.read_html(html)
+    table = data[0]
+    sliced_table = table[1:]
+    header = table.iloc[0]
+    corrected_table = sliced_table.rename(columns=header)
+    return corrected_table.values
+
+
+SP500 = {
+    "getSP500": getSP500
+}
