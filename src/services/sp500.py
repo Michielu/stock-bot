8a9a333762a062576cd3882a72d5cf0f1560f24b
Michielu Menning <mich.menning@gmail.com>
2020-04-11 20:07:06 -0500

    Return sp500 as class

--- src/services/sp500.py
+++ src/services/sp500.py
@@ -2,6 +2,7 @@ import certifi
 import ssl
 import urllib.request as urlrq
 import pandas as pd
+from data_obj.MyStock import MyStock
 
 
 def getSP500():
@@ -13,7 +14,12 @@ def getSP500():
     sliced_table = table[1:]
     header = table.iloc[0]
     corrected_table = sliced_table.rename(columns=header)
-    return corrected_table.values
+    values = corrected_table.values
+    sp500 = []
+    for x in values:
+        sp500.append(MyStock(x[0], x[1], x[3]))
+
+    return sp500
 
 
 SP500 = {
