76387e14df099687704301e244c2d14ed809e576
Michielu Menning <mich.menning@gmail.com>
2020-04-15 16:43:34 -0500

    Generic graph

--- src/util/graph.py
+++ src/util/graph.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -12,15 +13,30 @@ def basic_graph(data):
 
 
 def volume_graph(data):
-    print("data: ", data)
+    # DataFrame and series's index already has dates as index
     pd.DataFrame({
         "CLOSE": data['Close'],
         "Volume": data['Volume']
     }).plot(title="Volume Graph title")
+
+
+def graph(data_dic):
+    # Data_dic example:
+    # {
+    #     "SPY": data_dic["SPY"],
+    #     "Account": data_dic["Account"]
+    # }
+    # keys = data_dic.keys()
+    # for k in keys:
+    #     print(k, ": ", type(data_dic[k]), data_dic[k])
+
+    df = pd.DataFrame(data_dic)
+    df.plot(title="Yada yada Graph title")
     plt.show()
 
 
 Graph = {
     "basic_graph": basic_graph,
-    "volume_graph": volume_graph
+    "volume_graph": volume_graph,
+    "graph": graph
 }
