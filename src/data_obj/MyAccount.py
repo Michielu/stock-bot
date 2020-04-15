2cc371d4456ac6221a01e3c6f63852d0a7ad282b
Michielu Menning <mich.menning@gmail.com>
2020-04-14 20:44:29 -0500

    MyAccount to round and clean up

--- src/data_obj/MyAccount.py
+++ src/data_obj/MyAccount.py
@@ -1,77 +1,79 @@
 import numpy as np
 import pandas as pd
+from data_obj.StockLog import StockLog
 
 
 class MyAccount:
-    # {ticker: [num, average_price]}
-    stock_own = {}
+    # Array of StockLog objects
+    stocks_log_list = []
 
     def __init__(self, account_value):
         self.buying_power = account_value
 
     def get_account_value(self):
         # Iterate through stock
-        invested_stocks = 0
-        for s in self.stock_own:
-            num_price = self.stock_own[s]
-            invested_stocks += num_price[0]*num_price[1]
+        invested_in_stocks = 0
+        for s in self.stocks_log_list:
+            invested_in_stocks += s.get_total_invested()
 
-        print("Invested : ", invested_stocks,
-              "Buying power: ", self.buying_power)
-        return invested_stocks + self.buying_power
+        print("Invested : ", round(invested_in_stocks, 2),
+              "Buying power: ", self.get_buying_power())
+        return round(invested_in_stocks + self.buying_power, 2)
 
     def get_buying_power(self):
-        return self.buying_power
+        return round(self.buying_power, 2)
 
     # value = price per stock
     def buy_stock(self, ticker, num, value):
         # Check if enough buying power
+        if self.buying_power < num*value:
+            return "Not enough buying power"
 
-        if ticker in self.stock_own:
-            stock_revenue = self.stock_own[ticker]
-            total_investment = stock_revenue[0] * \
-                stock_revenue[1] + (num*value)
-            total_num = stock_revenue[0] + num
-            average = total_investment / total_num
-            self.stock_own[ticker] = [total_num, average]
-        else:
-            self.stock_own[ticker] = [num, value]
+        # Check if previous own stock
+        for stock_log in self.stocks_log_list:
+            if stock_log.get_ticker() == ticker:
+                stock_log.buy_stock(num, value)
+                return "Bought more"
+
+        self.stocks_log_list.append(StockLog(ticker, num, value))
 
         self.buying_power -= num*value
 
         print("Buying ", num, " of ", ticker, " for ", value, "/each")
-        return self.stock_own
+        return "Bought"
 
-    # value = price selling each stock
     def sell_stock(self, ticker, num, value):
-        # Check if ticker isn't owned
+        for stock_log in self.stocks_log_list:
+            if stock_log.get_ticker() == ticker:
+                status = stock_log.sell_stock(num)
+                if stock_log.get_num_purchased() == 0:
+                    self.stocks_log_list.remove(stock_log)
+                print(status)
 
-        # Don't calculate new average
-        # owned_after = self.stock_own[ticker][0] - num
-        # # calculate new average
-        # before_sell_amount_invested = self.stock_own[ticker][0] * \
-        #     self.stock_own[ticker][1]
-        # after_sell_amount_invested = before_sell_amount_invested - \
-        #     (num * value)
-        # print(before_sell_amount_invested, " ", after_sell_amount_invested)
-        # new_average = after_sell_amount_invested / owned_after
-
-        if(self.stock_own[ticker][0] == num):
-            del self.stock_own[ticker]
-            print("Sold all (", num, ") ", ticker, " for ", num*value)
-        else:
-            self.stock_own[ticker][0] -= num
-            print("Sold ", num, " ", ticker, " for ", num*value)
+        if status != "Successfully sold":
+            raise Exception("Error Selling stock")
 
         self.buying_power += num*value
+        return status
+
+    def owned_stock_info(self, ticker):
+        for stock_log in self.stocks_log_list:
+            if stock_log.get_ticker() == ticker:
+                return stock_log.get_data()
+
+        raise Exception("Cannot sell unowned stock")
 
     def print_summary(self):
         print("")
         print("========================================")
         print("Account Value: ", self.get_account_value())
-        print("Buying power: ", self.buying_power)
-        if self.stock_own:
-            stock_df = pd.DataFrame(self.stock_own)
+        print("Buying power: ", self.get_buying_power())
+
+        # convert to dataframe
+
+        if self.stocks_log_list:
+            stock_df = pd.DataFrame.from_records(
+                [stock_log.get_data() for stock_log in self.stocks_log_list])
             print(stock_df)
         print("========================================")
         print("")
