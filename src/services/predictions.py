98092d5e32bc4dd5d7aab8b5bd685f9309494493
Michielu Menning <mich.menning@gmail.com>
2020-04-16 19:54:32 -0500

    Add error catching in predictions

--- src/services/predictions.py
+++ src/services/predictions.py
@@ -4,14 +4,31 @@ import stocker as Stocker
 def get_predictions(my_stocks_arr):
     i = 0
     prediction_info = {}
+    num = len(my_stocks_arr)
+    # print(my_stocks_arr)
+    # for company in my_stocks_arr:
+    #     print(company.get_data())
+
     for company in my_stocks_arr:
-        if(i > 10):
-            break
-        future_prediction = Stocker.predict.tomorrow(company.get_ticker())
-        company.set_predictions(future_prediction)
+        # if(i > 10):
+        #     break
+        ti = company.get_ticker()
+        # ti = company
+        future_prediction = []
+        try:
+            future_prediction = Stocker.predict.tomorrow(ti)
+        except:
+            i += 1
+            print("Error with ", ti)
+            continue
+
+        # todo: check if failed with user so it stops this
+
+        # company.set_predictions(future_prediction)
         i = i+1
-        prediction_info[company.get_ticker()] = future_prediction
-        print(company.get_ticker(), " ", future_prediction)
+        prediction_info[ti] = future_prediction
+        print(ti, " ", future_prediction)
+        print("Done", i, "/", num)
 
     return prediction_info
 
@@ -23,9 +40,9 @@ def calculate_ratio(predict, current):
     ratio_dic = {}
     for t in predict.keys():
         ratio = predict[t][0]/current[t]
-        print(t, " : ", ratio)
-        if ratio > 1.01:
-            ratio_dic[t] = ratio
+        # print(t, " : ", ratio)
+        # if ratio > 1.01:
+        ratio_dic[t] = ratio
         # print("Ratio:", ratio)
     return ratio_dic
 
@@ -37,9 +54,9 @@ def calculate_error(predict, current):
     error_dic = {}
     for t in predict.keys():
         ratio = predict[t][1]
-        print(t, " : ", ratio)
-        if ratio > 1.01:
-            error_dic[t] = ratio
+        # print(t, " : ", ratio)
+        # if ratio > 1.01:
+        error_dic[t] = ratio
         # print("Ratio:", ratio)
     return error_dic
 
