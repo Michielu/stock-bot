0b8a71b7714d177f1ee64b01b8c8528fd18ea3c8
Michielu Menning <mich.menning@gmail.com>
2020-04-21 20:31:27 -0500

    Price change helper function

--- src/util/parabolic_helper.py
+++ src/util/parabolic_helper.py
@@ -16,20 +16,27 @@ def get_sma_balance(values):
     return sum(values)*weight
 
 
-def get_price_change():
-    #Assert(PChangeLength > 0, "'length' must be positive: " + PChangeLength);
-    # def PriceChange = if ValueforPC[PChangeLength] != 0 then (ValueforPC / ValueforPC[PChangeLength] - 1) * 100 else 0;
-    print('price change!')
+def get_price_change(high, low, past_price_change):
+    if past_price_change == None:
+        return 0
+    else:
+        hl2 = get_hl2(high, low)
+        if hl2 == None:
+            print("Error getting hl2")
+            return 0
+
+        return (hl2/past_price_change - 1)*100
 
 
 def get_hl2(high, low):
-    if low > 0:
-        return -1
+    if low == 0:
+        return None
     return round(high/low, 2)
 
 
 PARABOLIC_HELPER = {
-    "get_sma_balance": get_sma_balance
+    "get_sma_balance": get_sma_balance,
+    "get_price_change": get_price_change
 }
 
 # Tests
