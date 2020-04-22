ccdfe446d71dd1bdb6e45174297db8fac29bf0d0
Michielu Menning <mich.menning@gmail.com>
2020-04-21 20:32:01 -0500

    MyStock using sma and past stock price

--- src/module_obj/MyStock.py
+++ src/module_obj/MyStock.py
@@ -1,10 +1,16 @@
 import numpy as np
+from util.parabolic_helper import PARABOLIC_HELPER
 
 
 class MyStock:
     prediction_price = -1
     prediction_error_chance = -1
     prediction_date = -1
+    history_price = []
+    history_sma = []
+    sma_window = 3
+    price_change_length = 14  # has to be a positive number
+    history_price_change = []
 
     def __init__(self, ticker, name, industry):
         self.ticker = ticker
@@ -34,9 +40,43 @@ class MyStock:
     def get_prediction_date(self):
         return self.predictionDate
 
+    def get_history_price(self):
+        return self.history_price
+
+    def get_history_sma(self):
+        return self.history_sma
+
     def get_data(self):
         return np.array([self.ticker, self.name, self.industry, self.prediction_price, self.prediction_error_chance, self.prediction_date])
 
+    def add_stock_price(self, price):
+        self.history_price.append(price)
 
-# stock1 = MyStock("MMM", "3M Industry", "Manuafactorying")
-# print(stock1.getData())
+        sma = self.get_sma(self.sma_window)
+        if sma != None:
+            self.history_sma.append(sma)
+
+        pc = self.get_price_change(high, low)
+        if pc != None:
+            self.history_price_change.append(pc)
+
+    def get_sma(self, window):
+        # TODO maybe store some placeholder values in sma
+        if len(self.history_price) < window:
+            return None
+
+        print(window, self.history_price[-window:])
+        return PARABOLIC_HELPER["get_sma_balance"](self.history_price[-window:])
+
+    def get_price_change(self, high, low):
+        # Test what happens when price_change_length gets an out of index value
+
+        if self.price_change_length <= len(self.history_price_change):
+            past_price_change = self.history_price_change[-self.price_change_length]
+        else:
+            past_price_change = None
+
+        return PARABOLIC_HELPER["get_price_change"](high, low, past_price_change)
+
+
+# Test
