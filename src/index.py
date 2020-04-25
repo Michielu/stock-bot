61cbc774c76569e56ad410cf711128897908b020
Michielu Menning <mich.menning@gmail.com>
2020-04-24 20:14:15 -0500

    Clean index

--- src/index.py
+++ src/index.py
@@ -26,7 +26,7 @@ real_time = False
 time_frame = "5d"
 
 ############### Choose Account ###############
-account = MyAccount(500)
+account = MyAccount(2500)
 if real_brokerage:
     account = RHAccount(20000)  # TODO won't be inputting value
 
@@ -43,7 +43,8 @@ if real_time:
     # TODO make it only run during market hours
     while True:
         current_price = Current["get_current_price"]("SPY")
-        account_balance = strat.next_data_point("SPY", current_price)
+        account_balance = strat.next_data_point(
+            "SPY", current_price, pd.datetime.now())
         graph_data["Account"].append(account_balance)
         graph_data["SPY"].append(current_price)
         print("Graph_data: ", graph_data)
@@ -54,26 +55,17 @@ else:
     # data_df = History["get_data"]("SPY", time_frame)
     data_df = History["tommich_test_data"]("SPY")
     graph_data = {}
-    # graph_data["SPY"] = data_df["Close"]  # data_df["Close"]
     graph_data["Account"] = []
     for index, row in data_df.iterrows():
-        account_balance = strat.next_data_point("SPY", row)
+        account_balance = strat.next_data_point("SPY", row, index)
         graph_data["Account"].append(account_balance)
 
 
 print("FINISHED!")
+print(account.print_summary())
 # print("Graph data", graph_data)
-# Graph["graph"](graph_data)
+Graph["graph"](graph_data)
 
-# account.print_summary()
-
-
-# Strategy["stop_loss"](account, day_data_df)
-
-# Practice Graphing
-# yearData = History["get_history_data"]("MSFT")
-# print(yearData)
-# Graph["basic_graph"](yearData)
 
 # day_data_df = History["get_week_data"]("SPY")
 # print(type(day_data_df))
