aab9e0b0af671377fe34e1ea734060c6c426eadc
Michielu Menning <mich.menning@gmail.com>
2020-04-26 12:29:32 -0500

    Clean up Index file

--- src/index.py
+++ src/index.py
@@ -1,17 +1,9 @@
 # pylint: disable=import-error, no-name-in-module
-# https://towardsdatascience.com/stock-analysis-in-python-a0054e2c1a4c
-# Issue with Catalina OS https://github.com/tensorflow/tensorflow/issues/33183
-# Another solution: https://apple.stackexchange.com/questions/237430/how-to-install-specific-version-of-python-on-os-x/319675#319675
-# yfinance limit: 2000 calls/hour
-# Robinhood: https://github.com/JGrauPirozzi/robinhood
+
 from util.graph import Graph
-from test.test_account import test_account
-from strategy.practice import PracticeStrat
 from strategy.tommich import Tommich
-from strategy.stop_loss import Strategy
 from data_source.current import Current
 from data_source.history import History
-from module_obj.MyStock import MyStock
 from module_obj.Account.RHAccount import RHAccount
 from module_obj.Account.MyAccount import MyAccount
 import pandas as pd
@@ -31,13 +23,12 @@ if real_brokerage:
     account = RHAccount("mich.menning@gmail.com")
 
 ############### Choose Strategy ###############
-# strat = PracticeStrat(account, "SPY")
 strat = Tommich(account, "SPY")
 
 if real_time:
     print("Do real time stuff")
     graph_data = {}
-    graph_data["SPY"] = []  # data_df["Close"]
+    graph_data["SPY"] = []
     graph_data["Account"] = []
 
     # TODO make it only run during market hours
@@ -61,13 +52,5 @@ else:
         graph_data["Account"].append(account_balance)
 
 
-print("FINISHED!")
 print(account.print_summary())
-# print("Graph data", graph_data)
 Graph["graph"](graph_data)
-
-
-# day_data_df = History["get_week_data"]("SPY")
-# print(type(day_data_df))
-# print(day_data_df[["Close"]])
-# Graph["volume_graph"](day_data_df)
