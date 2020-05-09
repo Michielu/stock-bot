0be091217dcc5eb966910d0fe2542642f9330612
Michielu Menning <mich.menning@gmail.com>
2020-05-08 20:16:31 -0500

    Add more stock information for Tommich strat

--- src/module_obj/MyStock.py
+++ src/module_obj/MyStock.py
@@ -17,6 +17,14 @@ class MyStock:
     history_roc = []
     history_parabolic_trend = []
     history_hl2 = []
+    history_ohlc4 = []
+    tema_long = 7
+    tema_short = 1
+    tema_boundry = 1
+    history_tema_short = []
+    history_tema_long = []
+    history_tema_boundry = []
+    history_trend_quality = []
 
     def __init__(self, ticker):
         self.ticker = ticker
@@ -47,9 +55,12 @@ class MyStock:
         if sma != None:
             self.history_sma.append(sma)
 
-        # hl2 = self.gen_hl2(high_price, low_price)
-        # if hl2 != None:
-        #     self.history_hl2.append(hl2)
+        hl2 = self.gen_hl2(high_price, low_price)
+        if hl2 != None:
+            self.history_hl2.append(hl2)
+
+        self.history_ohlc4.append(self.gen_ohlc4(
+            open_price, high_price, low_price, closing_price))
 
         pc = self.gen_roc(closing_price)
         if pc != None:
@@ -57,6 +68,13 @@ class MyStock:
 
         self.history_parabolic_trend.append(
             self.ParabolicTrend.next(high_price, low_price))
+
+        self.history_tema_short.append(self.gen_exp_average(
+            self.history_price_close, self.tema_short))
+        self.history_tema_long.append(self.gen_exp_average(
+            self.history_price_close, self.tema_long))
+        self.history_tema_boundry.append(
+            self.gen_exp_average(self.history_price_close, self.tema_boundry))
 
     def gen_sma(self, window):
         # TODO maybe store some placeholder values in sma
@@ -75,20 +93,25 @@ class MyStock:
             return self.history_sma[-2]
         return None
 
-    # def gen_hl2(self, high, low):
-    #     return TOMMICH_HELPER["get_hl2"](high, low)
+    def gen_hl2(self, high, low):
+        return TOMMICH_HELPER["get_hl2"](high, low)
+
+    def gen_ohlc4(self, open, high, low, close):
+        return (open+high+low+close)/4
+
+    def get_ohlc4(self):
+        return self.history_ohlc4[-1]
 
     def gen_roc(self, closing_price):
-
         roc = TOMMICH_HELPER["get_roc"](
             closing_price, self.history_price_close, self.roc_length)
         # print("Past rate of change :", roc)
 
         return roc
 
-    def get_roc(self):
-        if len(self.history_roc) > 0:
-            return self.history_roc[-1]
+    def get_roc(self, previous=-1):
+        if len(self.history_roc) >= abs(previous):
+            return self.history_roc[previous]
         return None
 
     def get_previous_roc(self):
@@ -99,9 +122,25 @@ class MyStock:
     def get_parabolic_trend(self):
         if len(self.history_parabolic_trend) > 0:
             return self.history_parabolic_trend[-1]
+        return 0  # We're using this value to subtract
+
+    def gen_exp_average(self, data, window):
+        return TOMMICH_HELPER["get_triple_exp_average"](data[-window:], window)[-1]
+
+    def get_tema_long(self, previous=-1):
+        if len(self.history_tema_long) >= abs(previous):
+            return self.history_tema_long[previous]
         return None
 
-    # def get_exp_average(self):
-    #     return TOMMICH_HELPER["get_triple_exp_average"]()
+    def get_tema_short(self, previous=-1):
+        if len(self.history_tema_short) >= abs(previous):
+            return self.history_tema_short[previous]
+        return None
+
+    def get_tema_boundry(self):
+        return self.history_tema_boundry[-1]
+
+    # def gen_trend_quality(self):
+    #     noise = self.correction_factor *
 
 # Test
